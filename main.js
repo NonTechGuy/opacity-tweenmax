f6ca99502817ca7e5cdaafe51bc83d1e237ccf7a
Craig Bartlett <craigbartlett@Craigs-MacBook-Pro.local>
2021-08-21 13:07:04 +0200

    pranaymajee original code

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-TweenMax.to(".loadcon",
+TweenMax.to(
+    ".loadcon",
     2, {
-    y: "-100%",
-    ease: Expo.easeInOut,
-    delay: 6,
-},
-);
-
-TweenMax.staggerTo(
+      y: "-100%",
+      ease: Expo.easeInOut,
+      delay: 6,
+    },
+  );
+  
+  
+  TweenMax.staggerFrom(
     ".loadtext",
     1, {
-    x: "80",
-    ease: Power3.easeInOut,
-    opacity: "1",
-},
+      x: "-80",
+      ease: Power3.easeInOut,
+      opacity: "0",
+    },
     2
-);
-TweenMax.staggerFrom(
+  );
+  TweenMax.staggerTo(
     ".loadtext",
     0.8, {
-    x: "-80",
-    ease: Power3.easeInOut,
-    delay: 1.2,
-    opacity: "0",
-},
+      x: "80",
+      ease: Power3.easeInOut,
+      delay: 1.2,
+      opacity: "0",
+    },
     2
-);
+  );
+  
